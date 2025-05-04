db1236ddf3fdf63a1559786570858d87c65d1cb8
ionvop <ionvop@gmail.com>
2025-05-04 11:51:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import cv2
 import base64
 import requests
 import json
+import time
 
 
 config = json.load(open("config.json"))
 cap = cv2.VideoCapture(config["camera_ip"] + ":81/stream")
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 latest_frame = None
 is_running = True
+test = 0
 
 
 class API:
     def get_frame(self):
-        _, img_encoded = cv2.imencode(".jpg", latest_frame)
+        global test
+        _, img_encoded = cv2.imencode(".jpg", cv2.flip(latest_frame, 0))
+        print(test)
+        test += 1
         return base64.b64encode(img_encoded).decode()
     
 
     def process_results(self, result):
         print(result)
-        route = {"left": "L", "right": "R", "up": "U", "down": "D"}.get(result)
+        route = {"left": "L", "right": "R", "scan": "S"}.get(result)
 
         try:
             response = requests.get(config["servo_ip"] + f"/{route}", timeout=1)
@@ -28,11 +32,14 @@ class API:
         except requests.exceptions.RequestException as e:
             print(e)
 
+        time.sleep(0.1)
+        return True
+
 
 def main() -> None:
     global is_running
     webview.create_window("Smart Desk Lamp", "index.html", js_api=API())
-    webview.start(webview_thread)
+    webview.start(webview_thread, debug=True)
     is_running = False
 
 
